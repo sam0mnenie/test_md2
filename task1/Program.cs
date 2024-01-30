d11121a624ba2f498c7f9e207054a5863a568b8e
skaylean4 <skaylean444@gmail.com>
2024-01-30 14:34:13 +0200

    second commit

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Runtime.InteropServices;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello World!");
 
 int a = 11;
 int b = 12;
