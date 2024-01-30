4a372d85f1e658a96378cbb1d935db2253a65151
skaylean4 <skaylean444@gmail.com>
2024-01-30 14:31:50 +0200

    first commit

---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+
+int a = 11;
+int b = 12;
+Console.WriteLine(a + b);
