7fe2b7bad965d8bcad8eeb517e5074f81e38a56d
skaylean4 <skaylean444@gmail.com>
2024-01-30 14:46:52 +0200

    three commit

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,6 +3,11 @@ using System.Runtime.InteropServices;
 
 Console.WriteLine("Hello World!");
 
-int a = 11;
-int b = 12;
-Console.WriteLine(a + b);
+//int a = 11;
+//int b = 12;
+//Console.WriteLine(a + b);
+
+
+int a = 10;
+int c = 330;
+Console.WriteLine(a * c);
